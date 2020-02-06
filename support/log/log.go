121f61da1b136738cc5f3728a5f3756f76da25d8
hexing <hexing@yunify.com>
2020-02-06 19:27:31 +0800

    fix config log

--- support/log/log.go
+++ support/log/log.go
@@ -14,13 +14,13 @@ var (
 )
 
 type Option struct {
-	LogPath       string `json:"log_path"`
-	LogName       string `json:"log_name"`
-	LogLevel      Level  `json:"log_level"` //1 debug,2 info,3 warn,4 error,5 fatal
-	MaxSize       int    `json:"max_size"`
-	MaxBackup     int    `json:"max_backup"`
-	MaxAge        int    `json:"max_age"`
-	ConsoleFormat bool   `json:"console_format"`
+	LogPath       string `json:"logPath"`
+	LogName       string `json:"logName"`
+	LogLevel      Level  `json:"logLevel"` //1 debug,2 info,3 warn,4 error,5 fatal
+	MaxSize       int    `json:"maxSize"`  //日志分割的尺寸 MB
+	MaxBackup     int    `json:"maxBackup"`
+	MaxAge        int    `json:"maxAge"` //分割日志保存的时间 day
+	ConsoleFormat bool   `json:"consoleFormat"`
 }
 
 func Init(option *Option) {
