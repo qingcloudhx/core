eaa7575a719601746a5c2ff970d0c97613afa631
hexing <hexing@yunify.com>
2020-02-12 16:02:58 +0800

    fix config log

--- constant/log_field.go
+++ constant/log_field.go
@@ -10,3 +10,20 @@ const (
 const (
 	CommitLogSource_EdgeWize = "EdgeWize"
 )
+
+const (
+	//Monitor服务启动
+	LogStatus_MonitorStart = 920
+	//Monitor服务停止
+	LogStatus_MonitorStop = 921
+	//Monitor服务运行失败
+	LogStatus_MonitorFailed = 922
+	//采集数据成功
+	LogStatus_ColllectSuccess = 923
+	//采集数据失败
+	LogStatus_CollectFailed = 924
+	//上报数据成功
+	LogStatus_PostSuccess = 925
+	//上报数据失败
+	LogStatus_PostFailed = 926
+)
